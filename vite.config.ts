02e50354f05cba7fb702f222a8d012b39748c65f
JOAO-LUCIO <joao.silvarrr@gmail.com>
2026-07-22 12:07:49 -0400

    chore: adding aliases for imports

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@assets": "/src/assets",
+      "@components": "/src/components",
+      "@composables": "/src/composables",
+      "@i18n": "/src/i18n",
+      "@types": "/src/types",
+    },
+  },
 });
